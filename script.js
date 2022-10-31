a631b67b0d10457a38682fc821fa49bf1aa6eaa0
Marta Welfe-Starzak <mwelfe@gmail.com>
2022-10-31 16:06:11 +0100

    feat: gameboard_module

---
+++ script.js
@@ -0,0 +1,17 @@
+
+const gameboard_module = (function () {
+    let board = [];
+    for (let i = 0; i < 9; i++) {
+        let container = document.getElementById("board_container");
+        let board_element = document.createElement("div");
+        board_element.classList.add("board_element");
+        board_element.innerText = board[i];
+        board_element.addEventListener("click", onBoardClick);
+        container.appendChild(board_element);
+    }
+    return board;
+})();
+
+
+
+// onBoardClick dopisac funk
