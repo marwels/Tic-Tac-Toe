734948ad5876c108f4dd07751f25a16941dd7e99
Marta Welfe-Starzak <mwelfe@gmail.com>
2022-11-07 15:45:28 +0100

    feat: style elements

--- script.js
+++ script.js
@@ -16,9 +16,15 @@ const startGame_module = (function () {
         const gameContainer = document.getElementById("gameContainer");
         gameContainer.replaceChildren();
 
+        let divChoosePanel = document.createElement("div");
+        divChoosePanel.classList.add("divChoosePanel");
+
+
+
         let information = document.createElement("p");
         information.innerText = "Choose O or X for the first player";
-        gameContainer.appendChild(information);
+        information.classList.add("information");
+        divChoosePanel.appendChild(information);
 
 
         let buttonO = document.createElement("button");
@@ -31,8 +37,9 @@ const startGame_module = (function () {
         buttonX.textContent = "Play as X";
         buttonX.addEventListener("click", () => onPlayAs(1))
 
-        gameContainer.appendChild(buttonO);
-        gameContainer.appendChild(buttonX);
+        divChoosePanel.appendChild(buttonO);
+        divChoosePanel.appendChild(buttonX);
+        gameContainer.appendChild(divChoosePanel);
     };
 
     let resultClb;
@@ -56,7 +63,7 @@ const startGame_module = (function () {
 })();
 
 const gameboard_module = (function () {
-    let board = [];
+    let board = [-10, -10, -10, -10, -10, -10, -10, -10, -10];
     let player;
 
     function setPlayerPlaysAs(playerChoiceXOrO) {
@@ -64,14 +71,17 @@ const gameboard_module = (function () {
     }
 
     function showGameBoard(onRestart) {
-        board = [];
+        board = [-10, -10, -10, -10, -10, -10, -10, -10, -10];
         const gameContainer = document.getElementById("gameContainer");
         gameContainer.replaceChildren();
+        let divRestart = document.createElement("div");
+        divRestart.classList.add("restart");
         let restartButton = document.createElement("button");
         restartButton.classList.add("restart");
         restartButton.textContent = "Restart";
         restartButton.addEventListener("click", onRestart);
-        gameContainer.appendChild(restartButton);
+        divRestart.appendChild(restartButton);
+        gameContainer.appendChild(divRestart);
 
         let board_container = document.createElement("div");
         board_container.classList.add("board_container");
@@ -81,7 +91,13 @@ const gameboard_module = (function () {
             let board_element = document.createElement("div");
             board_element.classList.add("board_element");
             board_element.dataset['index'] = i;
-            board_element.innerText = board[i] || "";
+
+            if (board[i] === -10) {
+                board_element.innerText = "";
+            } else {
+                board_element.innerText = board[i];
+            }
+
             board_element.addEventListener("click", onBoardClick);
             console.log(board_element);
             board_container.appendChild(board_element);
@@ -96,16 +112,15 @@ const gameboard_module = (function () {
 
         function checkIfSBwon() {
             console.log(board);
-            console.log(board[0] * board[1] * board[2]);
-            console.log(board[2] * board[5] * board[8]);
-            if (!(Number.isNaN(board[0] * board[1] * board[2])) ||
-                !(Number.isNaN(board[3] * board[4] * board[5])) ||
-                !(Number.isNaN(board[6] * board[7] * board[8])) ||
-                !(Number.isNaN(board[0] * board[3] * board[6])) ||
-                !(Number.isNaN(board[1] * board[4] * board[7])) ||
-                !(Number.isNaN(board[2] * board[5] * board[8])) ||
-                !(Number.isNaN(board[0] * board[4] * board[8])) ||
-                !(Number.isNaN(board[2] * board[4] * board[6]))) {
+
+            if ((board[0] + board[1] + board[2] >= 0) ||
+                (board[3] + board[4] + board[5] >= 0) ||
+                (board[6] + board[7] + board[8] >= 0) ||
+                (board[0] + board[3] + board[6] >= 0) ||
+                (board[1] + board[4] + board[7] >= 0) ||
+                (board[2] + board[5] + board[8] >= 0) ||
+                (board[0] + board[4] + board[8] >= 0) ||
+                (board[2] + board[4] + board[6] >= 0)) {
                 if ((board[0] + board[1] + board[2] === 0) ||
                     (board[3] + board[4] + board[5] === 0) ||
                     (board[6] + board[7] + board[8] === 0) ||
@@ -118,6 +133,7 @@ const gameboard_module = (function () {
                     alertO.classList.add("alert");
                     alertO.innerText = "O WON!";
                     gameContainer.appendChild(alertO);
+                    return;
                 } else if (
                     (board[0] + board[1] + board[2] === 3) ||
                     (board[3] + board[4] + board[5] === 3) ||
@@ -131,7 +147,8 @@ const gameboard_module = (function () {
                     alertX.classList.add("alert");
                     alertX.innerText = "X WON!";
                     gameContainer.appendChild(alertX);
-                } else if (!(board.includes(undefined))) {
+                    return;
+                } else if (!(board.includes(-10))) {
                     let alertTIE = document.createElement("div");
                     alertTIE.classList.add("alert");
                     alertTIE.innerText = "IT'S A TIE!";
